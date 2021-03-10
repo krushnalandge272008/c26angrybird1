17f1fce434fef0185888628cebfda8b2560c954a
krushnalandge272008 <krushnalandge272008@gmail.com>
2021-03-10 16:51:01 +0530

    add pig

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+    pig4= new Pig(30,30);
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    pig4.display();
 }
